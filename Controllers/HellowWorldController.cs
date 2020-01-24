a60c6a042569f1af569bbbc6349b060364482fec
Mohsen Mahdavi <Mosen.IT@live.com>
2020-01-24 18:09:01 +0800

    initial commit

---
+++ Controllers/HellowWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string WelcomeMessage(string name, int numTimes = 1)
+        {            
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {numTimes }");
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
